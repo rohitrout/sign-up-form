8b1c0a76d15b816bcd3fc724f5039e399bc075b0
Rohit Rout <rohitrout@outlook.com>
2023-04-01 17:26:52 +0530

    updated code

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
-import logo from './logo.svg';
 import './App.css';
-
+import { str } from './helper';
+import { useState } from 'react';
 function App() {
+  const [ captcha, setCaptcha ] = useState('')
+  const [ verified, setVerified ] = useState('')
+  const [ theme, setTheme ] = useState(false)
+
+  const handleCaptcha = (e) => {
+    setCaptcha(e.target.value)
+    console.log(captcha)
+  }
+
+  const handleCheck = () => {
+    if(captcha===str){
+      setVerified(true)
+      alert('Verified')
+    }else {
+      setVerified(false)
+      alert('Failed')
+    }
+  }
+
+  const handleTheme = () => {
+    setTheme(!theme)
+    
+    // style={{backgrounColor:'black'}}
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className={theme ? 'dark' : 'light'}>
+      {/* <div className='dark'> */}
+
+      <button onClick={()=>{handleTheme()}}>Theme Toggle</button>
+
+      <form className='form' onSubmit={(e)=>e.preventDefault()}>
+        <div> <label>First Name</label> <input/></div>
+        <div> <label>Last Name</label> <input/></div>
+        <div> <label>Email</label> <input/></div>
+        <div> <label >Captcha</label> <input value={captcha} onChange={(e) => handleCaptcha(e)}/>  </div> <span className='captcha'>{str}</span>
+        <button onClick={()=>{ handleCheck()}}>SIGN UP</button>
+      </form>
+
+      </div>
   );
 }
 
---
+++ src/helper.js
@@ -0,0 +1,1 @@
+export const str = (Math.random()+1).toString(16).substr(2,5)
